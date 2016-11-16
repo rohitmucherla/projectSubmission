87f74aeec0245bf3d5593f6530d654389a635d8a
Rohit Mucherla <rohitmucherla@dhcp-10-202-85-197.tamulink.tamu.edu>
2016-11-15 19:05:55 -0600

    Added all the files that we created during the meeting with the scaffold command as a base for the project

--- projectSubmission/config/routes.rb
+++ projectSubmission/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :project_submission_pages
+  resources :user_requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

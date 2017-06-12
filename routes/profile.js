3a5b95939d15db7db958aaa1cd02d1de9dd94577
Vikas <potlurivikas@gmail.com>
2017-06-12 18:32:59 -0500

    Forgot to commit profile.js somehow

--- routes/profile.js
+++ routes/profile.js
@@ -2,15 +2,103 @@
 * Profile Page
 */
 const express = require('express'),
-	router = express.Router();
+	router = express.Router(),
+	LIMIT = 10;
+
+Application = require('../application');
+Project = require('../project');
+
+router.use(checkLogin)
 
 router.get('/',function(req, res)
 {
-	//If the user is login, render the profile; otherwise have them login
-	if(req.user)
-		res.render('profile',{user:req.user})
-	else
-		res.redirect('/auth/google')
+	res.render('profile',{user:req.user})
 });
+
+router.get('/applications',function(req,res)
+{
+	Application.count().exec().then(function(number)
+	{
+		if(number > 0)
+		{
+			Application.find({"user-id":req.user.gid})
+				.limit(LIMIT)
+				.lean() //returns as Object instead of Instance
+				.exec()
+				.then(function(applications)
+			{
+				var projects = [];
+				applications.forEach(function(app)
+				{
+					console.log(app);
+					projects.push(app["project-id"]);
+				});
+				Project.find().where('id').in(projects).select('name id').exec().then(function(project)
+				{
+					_projects = {};
+					sendApp = [];
+					project.forEach(function(info)
+					{
+						_projects[info.id] = info.name;
+					});
+					applications.forEach(function(app,index)
+					{
+						app.projectName = _projects[app["project-id"]];
+					});
+					res.locals.applications = applications;
+					res.locals.pagination = (number > LIMIT) ?
+						{needed:true, number: Math.ceil(number / LIMIT), current:1} :
+						{needed:false};
+					res.render('profile-application-list')
+				}).catch((error)=>{res.status(500).render('error',{error:error})});
+			}).catch((error)=>{res.status(500).render('error',{error:error})});
+		}
+		else
+		{
+			res.render('application-404');
+		}
+	}).catch((error)=>{res.status(500).render('error',{error:error});});
+});
+
+router.get('/application/:id',function(req,res)
+{
+	res.redirect(`/profile/application/${req.params.id}/view`);
+});
+
+router.get('/application/:id/edit',function(req,res)
+{
+	Application.findOne().where('user-id').in(req.user.gid).where('project-id').in(req.params.id).exec().then(function(application)
+	{
+		if(application)
+		{
+			res.send('ok');
+		}
+		else
+		{
+			res.render('application-404',{single:true})
+		}
+	})
+})
+
+function checkLogin(req,res,next)
+{
+	console.log('checklogin');
+	//Passport middleware adds user to the req object. If it doesn't exist, the client isn't logged in
+	if(!req.user)
+	{
+		//Set redirect URL to the requested url
+		req.session.redirectTo = req.originalUrl;
+		//Redirect to the Google Authentication page
+		res.redirect('/auth/google');
+	}
+	else
+	{
+		//Admins have access > 10
+		if(!req.user.approved)
+			res.render('approval-needed',{path:req.originalUrl});
+		else
+			next();
+	}
+}
 
 module.exports = router;

1d6e0c6157fd619fe7d7adcc146980fe6bf511b6
Vikas <potlurivikas@gmail.com>
2017-07-31 01:33:40 -0500

    Updates to project listing for admin
    - Approved, unapproved and all are now available
    - Add tabs to project-listing view to switch
    - Don't show the unapproved info icon to admins

--- routes/admin/projects.js
+++ routes/admin/projects.js
@@ -11,6 +11,131 @@ router.get('/',function(req, res)
 		{
 			res.locals[key] = projectData[key];
 		}
+		res.locals.active = {
+			all:true
+		};
+		res.locals.headerTitle = "Projects";
+		res.locals.title = "Projects";
+		res.render('project-listing');
+	}).catch(function(error)
+	{
+		if(error == 'NO_PROJECTS_FOUND')
+		{
+			res.render('project-404');
+		}
+		else
+		{
+			res.status(500).render('error',{error:error});
+		}
+	});
+});
+
+router.get('/approved',function(req,res)
+{
+	loop(1,config.LIMIT,null,false,false,1).then(function(projectData)
+	{
+		for(key in projectData)
+		{
+			res.locals[key] = projectData[key];
+		}
+		res.locals.active = {
+			approved:true
+		};
+		res.locals.headerTitle = "Approved Projects";
+		res.locals.title = "Approved Projects";
+		res.render('project-listing');
+	}).catch(function(error)
+	{
+		if(error == 'NO_PROJECTS_FOUND')
+		{
+			res.render('project-404');
+		}
+		else
+		{
+			res.status(500).render('error',{error:error});
+		}
+	});
+});
+
+router.get('/approved/:offset',function(req,res)
+{
+	if(!parseInt(req.params.offset))
+	{
+		res.status(404).render('404');
+		return;
+	}
+
+	loop(req.params.offset,config.LIMIT,null,false,false,1).then(function(projectData)
+	{
+		for(key in projectData)
+		{
+			res.locals[key] = projectData[key];
+		}
+		res.locals.active = {
+			approved:true
+		};
+		res.locals.headerTitle = "Approved Projects";
+		res.locals.title = "Approved Projects";
+		res.render('project-listing');
+	}).catch((error) =>
+	{
+		if(error == 'NO_PROJECTS_FOUND')
+		{
+			res.render('project-404');
+		}
+		else
+		{
+			res.status(500).render('error',{error:error});
+		}
+	});
+});
+
+router.get('/unapproved',function(req,res)
+{
+	loop(1,config.LIMIT,null,false,false,0).then(function(projectData)
+	{
+		for(key in projectData)
+		{
+			res.locals[key] = projectData[key];
+		}
+		res.locals.active = {
+			unapproved:true
+		};
+		res.locals.headerTitle = "Unapproved Projects";
+		res.locals.title = "Unapproved Projects";
+		res.render('project-listing');
+	}).catch(function(error)
+	{
+		if(error == 'NO_PROJECTS_FOUND')
+		{
+			res.render('project-404');
+		}
+		else
+		{
+			res.status(500).render('error',{error:error});
+		}
+	});
+});
+
+router.get('/unapproved/:offset',function(req,res)
+{
+	if(!parseInt(req.params.offset))
+	{
+		res.status(404).render('404');
+		return;
+	}
+
+	loop(req.params.offset,config.LIMIT,null,false,false,0).then(function(projectData)
+	{
+		for(key in projectData)
+		{
+			res.locals[key] = projectData[key];
+		}
+		res.locals.active = {
+			unapproved:true
+		};
+		res.locals.headerTitle = "Unapproved Projects";
+		res.locals.title = "Unapproved Projects";
 		res.render('project-listing');
 	}).catch((error) =>
 	{
@@ -33,12 +158,17 @@ router.get('/:offset',function(req,res)
 		return;
 	}
 
-	loop(1,config.LIMIT,null,false,true).then(function(projectData)
+	loop(req.params.offset,config.LIMIT,null,false,true).then(function(projectData)
 	{
 		for(key in projectData)
 		{
 			res.locals[key] = projectData[key];
 		}
+		res.locals.active = {
+			all:true
+		};
+		res.locals.title = "Projects";
+		res.locals.headerTitle = "Projects";
 		res.render('project-listing');
 	}).catch((error) =>
 	{

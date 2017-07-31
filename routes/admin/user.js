2ff7c2ef00e538be1e6570449ecdebc61f0f0f76
Vikas <potlurivikas@gmail.com>
2017-07-31 13:34:26 -0500

    Updates to admin users route
    - Normailize render call
    - Not unverified, unapproved
    -- finish route
    - Add approved route
    - Add search route

--- routes/admin/user.js
+++ routes/admin/user.js
@@ -14,23 +14,86 @@ router.get('/',function(req,res)
 		.exec()
 		.then(function(users)
 	{
-		res.render('admin-users',{users:users});
-	}).catch(config.db.onError);
+		res.locals.users = users;
+		res.render('admin-users');
+	}).catch((e)=>{res.status(500).render('error',{error:e})});
 });
 
 //List users who have created an account but have not been approved by admins
-router.get('/unverified',function(req,res)
+router.get('/unapproved',function(req,res)
 {
 	User.find()
+		.where('approved').equals(0)
 		.lean()
-		.where('approved').equal(0)
 		.select('gid pic name email approved')
-		.limit(config.LIMIT)
 		.exec()
 		.then(function(users)
 	{
-		res.render('admin-users',{users:users,type:"Unverified"});
+		if(users.length)
+		{
+			res.locals.users = users;
+			res.locals.type = "Unapproved";
+			res.render('admin-users');
+		}
+		else
+		{
+			res.render('user-404');
+		}
 	}).catch((error)=>{res.status(500).render('error',{error:error})});
 });
+
+router.get('/approved',function(req,res)
+{
+	User.find()
+		.where('approved').equals(1)
+		.lean()
+		.select('gid pic name email approved')
+		.exec()
+		.then(function(users)
+	{
+		if(users.length)
+		{
+			res.locals.users = users;
+			res.locals.type = "Approved";
+			res.render('admin-users');
+		}
+		else
+		{
+			res.render('user-404');
+		}
+	}).catch((error)=>{res.status(500).render('error',{error:error})});
+});
+
+router.get('/search',function(req,res)
+{
+	if(!req.query.query)
+	{
+		res.locals.type = 'Search for'
+		res.render('admin-users');
+	}
+	else
+	{
+		search = new RegExp(req.query.query, "i");
+		User.find()
+		.or([{'name.first':search},{'name.last':search},{'name.full':search}])
+		.lean()
+		.select('gid pic name email approved')
+		.exec()
+		.then(function(users)
+		{
+			if(users.length)
+			{
+				res.locals.users = users;
+				res.locals.type = "Search for";
+				res.locals.search = req.query.query;
+				res.render('admin-users');
+			}
+			else
+			{
+				res.render('user-404');
+			}
+		}).catch((error)=>{res.status(500).render('error',{error:error})});
+	}
+})
 
 module.exports = router;

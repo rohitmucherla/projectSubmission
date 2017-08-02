1e7d7202b97e8dc5ccb81477bcd68126fd81b0b3
Vikas <potlurivikas@gmail.com>
2017-08-02 17:30:03 -0500

    Add project rejection route

--- routes/admin/project.js
+++ routes/admin/project.js
@@ -83,4 +83,56 @@ router.post('/:id/approve',function(req,res)
 	}).catch((e)=>{res.status(500).render('error',{error:e})});
 });
 
+router.get('/:id/reject',function(req,res)
+{
+	res.locals.item = {lower:'project',upper:'Project'};
+	res.render('wait-csrf-reject');
+});
+
+router.post('/:id/reject',function(req,res)
+{
+	let id = config.functions.mongooseId(req.params.id);
+	if(!id)
+	{
+		res.locals.single = true;
+		res.locals.projects = [];
+		res.render('project-listing');
+		return;
+	}
+	Project.findById(id)
+		.populate('owners','name email slack')
+		.exec()
+		.then(function(project)
+	{
+		let notes = false;
+		if(req.body.why)
+		{
+			notes = config.functions.sanitize(req.body.why);
+			project.statusNotes = notes;
+		}
+		if(!project || project.status > 0)
+		{
+			res.locals.project = [];
+			res.render('project-listing');
+			return;
+		}
+		project.owners.forEach(function(user)
+		{
+			let options = {
+				to: `"${user.name.full.replace(/"/g,'"')}" <${user.email}>`,
+				subject: `Your Project "${project.name}" has been rejected`,
+				body: `Hey ${user.name.first},<br/><br/> We just wanted to let you know that your project "${project.name}" has been rejected. The admin that rejected this project said the following:<br/> ${notes || '[nothing]'}<br/><br/>- The Project Submission Bot`
+			}
+			mailer(options).catch((e)=>{console.error(`Failed to send email to ${user.email} (${user._id}) - project approval - ${project._id}`)});
+		});
+		project.status = -2;
+		project.save().then(function()
+		{
+			req.flash('success',`Project "${project.name}" Rejected!`);
+			res.redirect('/admin/projects');
+		}).catch((e)=>res.status(500).render('error',{error:e}));
+
+	}).catch((e)=>{res.status(500).render('error',{error:e})});
+});
+
 module.exports = router;
